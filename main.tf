da6df9a46a7899e9099b1b9c1279197e35d8ffd1
Riyaz <mfriyaz@gmail.com>
2024-07-10 16:59:24 +0800

    Commit Ec2 with Security Group

--- main.tf
+++ main.tf
@@ -1,4 +1,30 @@
-resource "aws_instance" "MyEC2_01" {
-  ami           = "ami-0b287aaaab87c114d"
-  instance_type = "t2.nano"
+
+resource "aws_security_group" "rdp" {
+  name        = "allow_rdp"
+  description = "Allow RDP inbound traffic"
+  
+  ingress {
+    from_port   = 3389
+    to_port     = 3389
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "windows" {
+  ami           = "ami-0c55b159cbfafe1f0" # Change to a valid Windows AMI ID in your region
+  instance_type = "t2.micro"
+  key_name      = aws_key_pair.Chearit_EC201.key_name
+  security_groups = [aws_security_group.rdp.name]
+  
+  tags = {
+    Name = "WindowsServer"
+  }
 }
