c653fc2d30df4691f0aebd8db20b23aea2c3e7f1
Riyaz <mfriyaz@gmail.com>
2024-07-13 15:13:11 +0800

    02_Commit

--- main.tf
+++ main.tf
@@ -5,23 +5,4 @@ resource "aws_instance" "MyEC2" {
   tags = {
     Name = "Amazon Linux"
   }
-
-resource "aws_security_group" "rdp" {
-  name        = "allow_rdp"
-  description = "Allow RDP inbound traffic"
-  
-  ingress {
-    from_port   = 3389
-    to_port     = 3389
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
 }
