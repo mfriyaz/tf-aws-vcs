275812f318ce03cfe499e0b3b61af7527ceff17a
Riyaz <mfriyaz@gmail.com>
2025-01-22 11:42:52 +0800

    Ec2 type change

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "MyEC2" {
   ami           = "ami-0e97ea97a2f374e3d" # Change to a valid Windows AMI ID in your region
-  instance_type = "t2.nano"
+  instance_type = "t2.micro"
    
   tags = {
     Name = "Amazon Linux"
