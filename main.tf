ccdafc2f38b868ddbee2a88e140210d681f0a5b9
Riyaz <mfriyaz@gmail.com>
2025-01-24 00:59:03 +0800

    VPC Update

--- main.tf
+++ main.tf
@@ -1,8 +1,13 @@
-resource "aws_instance" "MyEC2" {
+/*resource "aws_instance" "MyEC2" {
   ami           = "ami-0e97ea97a2f374e3d" # Change to a valid Windows AMI ID in your region
   instance_type = "t2.micro"
    
   tags = {
     Name = "Amazon EC2 Linux"
   }
+}
+*/
+module "network" {
+  source = "./modules/network"
+  vpc_cidr = "10.0.0.0/16"   
 }
