46d4bfd1cfa576b4bb476cd6b56279cf6150dec1
Riyaz <mfriyaz@gmail.com>
2025-02-10 19:38:15 +0800

    Tag name update

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -1,13 +1,13 @@
-resource "aws_vpc" "main" {
+resource "aws_vpc" "main_01" {
     cidr_block = var.vpc_cidr
     enable_dns_support = true
     enable_dns_hostnames = true  
 
     tags = {
-      Name = "Riyaz VPC"
+      Name = "Riyaz VPC01"
     }
 }
-resource "aws_subnet" "main" {
+resource "aws_subnet" "main_01" {
   vpc_id = aws_vpc.main.id
   cidr_block = var.subnet_cidr
   availability_zone = var.availability_zone
--- modules/network/variables.tf
+++ modules/network/variables.tf
@@ -1,4 +1,12 @@
 variable "vpc_cidr" {
     description = "CIDR block for the VPC"
-    type = string   
+    type = string       
+}
+
+variable "subnet_cidr" {
+    description = "CIDR block for subnet"
+    type = string  
+}
+variable "availability_zone" {
+    description = "AWS availability zone for subnet"  
 }
