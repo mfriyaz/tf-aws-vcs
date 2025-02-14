b3f53a4868eafc5303da140f599bf2106a6d2cac
Riyaz <mfriyaz@gmail.com>
2025-02-14 18:43:41 +0800

    Compute outpud value updated

--- modules/compute/outputs.tf
+++ modules/compute/outputs.tf
@@ -1,7 +1,7 @@
 output "instance_id" {
-  value = aws_instance.main.id
+  value = aws_instance.main_01.id
 }
 
 output "instance_public_ip" {
-  value = aws_instance.main.public_ip
+  value = aws_instance.main_01.public_ip
 }
