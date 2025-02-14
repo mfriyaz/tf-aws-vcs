ae6308d6be3a1177bbb5fd94ca24edb5eeb00c77
Riyaz <mfriyaz@gmail.com>
2025-02-14 18:50:11 +0800

    Network outpud value updated

--- modules/network/outputs.tf
+++ modules/network/outputs.tf
@@ -1,9 +1,9 @@
 output "vpc_id" {
-    value = aws_vpc.main_01.id  
+    value = aws_vpc.main_vpc.id  
 }
 
 output "Subnet_id" {
-    value = aws_subnet.main_01.id 
+    value = aws_subnet.main_sub01.id 
 }
 
 output "aws_security_group_id" {
