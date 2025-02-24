1f77db2c04886738c86340fe9a71fc7b38b0c614
Riyaz <mfriyaz@gmail.com>
2025-02-24 19:41:31 +0800

    removed resource

--- modules/compute/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "instance_id" {
-  value = aws_instance.main_01.id
-}
-
-output "instance_public_ip" {
-  value = aws_instance.main_01.public_ip
-}
--- modules/network/main.tf
+++ modules/network/main.tf
@@ -1,38 +1,31 @@
-resource "aws_vpc" "main_vpc" {
-    cidr_block = var.vpc_cidr
-    enable_dns_support = true
-    enable_dns_hostnames = true  
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr
+  enable_dns_hostnames = true
+  enable_dns_support = true
+  }
 
-    tags = {
-      Name = "Riyaz VPC01"
-    }
-}
-resource "aws_subnet" "main_sub01" {
-  vpc_id = aws_vpc.main_vpc.id
-  cidr_block = var.subnet_cidr
-  availability_zone = var.availability_zone
-  map_public_ip_on_launch = true
+  resource "aws_subnet" "main" {
+   vpc_id = aws_vpc.main.id
+   cidr_block = var.aws_subnet
+   availability_zone = var.availability_zone
+   map_public_ip_on_launch = true
+      }
+  
+  resource "aws_security_group" "main" {
+    vpc_id = aws_vpc.main.id
+    name = var.sg_name
 
-  tags = {
-    Name = "Riyaz Sub01"
-  }
+    ingress {
+      from_port = var.ingress_port
+      to_port = var.ingress_port
+      protoprotocol = "tcp"
+      cidr_block = var.ingress_cidr_blocks   
+       }
+     egress {
+      from_port = 0
+      to_port = 0
+      protocol = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+     }
+      }
   
-}
-resource "aws_security_group" "sg01" {
-  vpc_id = aws_vpc.main_vpc
-  name = var.sg_name
-  ingress {
-    from_port = var.ingress_port
-    to_port = var.ingress_port
-    protocol = "tcp"
-    cidr_blocks = var.ingress_cidr_blocks
-  }
-  egress {
-    from_port = 0
-    to_port = 0
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-    
-  }
-  
-}
